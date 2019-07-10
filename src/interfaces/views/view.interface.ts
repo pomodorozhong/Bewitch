0c57b40ac501e091074a3cedf5f72fe013393237
pomodorozhong <pomodorozhong@gmail.com>
2019-07-11 00:20:46 +0800

    Add "Welcome scene" related method.

--- src/interfaces/views/view.interface.ts
+++ src/interfaces/views/view.interface.ts
@@ -1,3 +1,18 @@
+import { IPresenter } from "../presenters/presenter.interface";
+
 export interface IView {
-    toSetCookieCount(n: number): void;
+    showSettingGear(): void;
+    hideSettingGear(): void;
+    showSettingPanel(): void;
+    hideSettingPanel(): void;
+
+    // Welcome scene
+    showBird(): void;
+    hideBird(): void;
+    showWitchPanel(): void;
+    hideWitchPanel(): void;
+
+    // Character Selection scene
+    
+
 }
